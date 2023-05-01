af4a85091930386db4f08999cffd497869d6a487
AliMusa04 <tu75nx176@code.edu.az>
2023-05-01 22:15:26 +0400

    remove all functionality

--- game.js
+++ game.js
@@ -7,18 +7,108 @@ let frame = 0;
 const allImage = new Image();
 allImage.src = "img/sprite.png";
 
-const background = {
-  sX: 123,
-  sY: 23,
-  sWidth: 213,
-  sHeight: 123,
-  x: 12,
-  y: 23,
+const bird = {
+  animation: [
+    { sX: 276, sY: 112 },
+    { sX: 276, sY: 139 },
+    { sX: 276, sY: 164 },
+    { sX: 276, sY: 139 },
+  ],
+  x: 50,
+  y: 150,
+  width: 34,
+  height: 26,
+  frame: 0,
+
+  draw: function () {
+    let bird = this.animation[this.frame];
+    canvasCont.drawImage(
+      allImage,
+      bird.sX,
+      bird.sY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  },
 };
+const background = {
+  sX: 0,
+  sY: 0,
+  width: 275,
+  height: 226,
+  x: 0,
+  y: canvasElem.height - 130,
+  draw: function () {
+    canvasCont.drawImage(
+      allImage,
+      this.sX,
+      this.sY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    canvasCont.drawImage(
+      allImage,
+      this.sX,
+      this.sY,
+      this.width,
+      this.height,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
+  },
+};
+const foreground = {
+  sX: 276,
+  sY: 0,
+  width: 224,
+  height: 112,
+  x: 0,
+  y: canvasElem.height - 35,
+
+  draw: function () {
+    canvasCont.drawImage(
+      allImage,
+      this.sX,
+      this.sY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    canvasCont.drawImage(
+      allImage,
+      this.sX,
+      this.sY,
+      this.width,
+      this.height,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
+  },
+};
+
 function paint() {
   //SKY COLOR SET
   canvasCont.fillStyle = "#70c5ce";
   canvasCont.fillRect(0, 0, canvasElem.width, canvasElem.height);
+
+  background.draw();
+  foreground.draw();
+  bird.draw();
 }
 
 function update() {}
