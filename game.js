651aa870994e75c9942eb8d773906ebe2eeedffd
AliMusa04 <tu75nx176@code.edu.az>
2023-05-02 17:05:10 +0400

    pipe and bird's physique

--- game.js
+++ game.js
@@ -2,9 +2,9 @@
 let canvasElem;
 let canvasElemHeight = 640;
 let canvasElemWidth = 360;
-let canvasCont;
+let canvasContxt;
 
-//BIRD setup
+//BIRD SETUP
 
 let birdWidth = 34;
 let birdHeight = 24;
@@ -18,12 +18,25 @@ let bird = {
   x: birdX,
   y: birdY,
 };
+//PIPES SETUP
+let pipesArr = [];
+let topPipe;
+let topPipeWidth = 64;
+let topPipeHeight = 512;
+let pipeX = canvasElemWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//GAME PHYSIQUE
+let velocityX = -2;
 
 window.onload = function () {
   canvasElem = document.getElementById("cnv");
   canvasElem.height = canvasElemHeight;
   canvasElem.width = canvasElemWidth;
-  canvasCont = canvasElem.getContext("2d");
+  canvasContxt = canvasElem.getContext("2d");
 
   // canvasCont.fillStyle = "green";
   // canvasCont.fillRect(bird.x, bird.y, bird.width, bird.height);
@@ -32,6 +45,58 @@ window.onload = function () {
   birdImg.src = "./img/flappybird.png";
 
   birdImg.onload = function () {
-    canvasCont.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    canvasContxt.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
   };
+
+  //PIPES IMG
+  topPipeImg = new Image();
+  topPipeImg.src = "./img/toppipe.png";
+
+  bottomPipeImg = new Image();
+  bottomPipeImg.src = "./img/bottompipe.png";
+
+  requestAnimationFrame(update);
+  setInterval(createPipe, 1500);
 };
+
+function update() {
+  requestAnimationFrame(update);
+  canvasContxt.clearRect(0, 0, canvasElemWidth, canvasElemHeight);
+
+  // draw bird
+  canvasContxt.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+  //DRAW PIPE
+  pipesArr.forEach((pipe) => {
+    pipe.x += velocityX;
+    canvasContxt.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+  });
+}
+
+function createPipe() {
+  let randomPipeY =
+    pipeY - topPipeHeight / 4 - Math.random() * (topPipeHeight / 2);
+
+  let space = canvasElem.height / 4.5;
+
+  console.log(space);
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomPipeY,
+    width: topPipeWidth,
+    height: topPipeHeight,
+    passed: false,
+  };
+
+  let bottomPipe = {
+    img: bottomPipeImg,
+    x: pipeX,
+    y: randomPipeY + topPipeHeight + space,
+    width: topPipeWidth,
+    height: topPipeHeight,
+    passed: false,
+  };
+  pipesArr.push(topPipe);
+  pipesArr.push(bottomPipe);
+}
